2c7d44cbf762c6666c8f01ec448dc4280fd2a413
TaeKyeongKim <rlaxorud1324@naver.com>
2022-09-08 17:35:53 +0900

    Resolved: Sudoku

--- Algorithm/Algorithm/main.swift
+++ Algorithm/Algorithm/main.swift
@@ -10,14 +10,23 @@ import Foundation
 print("Hello, World!")
 
 let test = LeetNum9()
-var array = [0,1,0,3,12]
-
+var array:[[Character]] = [["5","3",".",".","7",".",".",".","."],
+                                 ["6",".",".","1","9","5",".",".","."],
+                                 [".","9","8",".",".",".",".","6","."],
+                                 ["8",".",".",".","6",".",".",".","3"],
+                                 ["4",".",".","8",".","3",".",".","1"],
+                                 ["7",".",".",".","2",".",".",".","6"],
+                                 [".","6",".",".",".",".","2","8","."],
+                                 [".",".",".","4","1","9",".",".","5"],
+                                 [".",".",".",".","8",".",".","7","9"]]
 var array2 = [1]
 //print(removeDuplicates(&array))
-
+//var arr = Array(repeating: Set<Character>(), count: 9)
 //rotate(&array, 2)
 //print(containsDuplicates(array))
 //print(intersect(array, array2))
 //print(moveZeroes(&array))
+//print(twoSum(array, 4))
+print(isValidSudoku(array))
 
-print(twoSum(array, 4))
+
---
+++ Algorithm/Algorithm/validSudoKu.swift
@@ -0,0 +1,70 @@
+//
+//  validSudoKu.swift
+//  Algorithm
+//
+//  Created by Kai Kim on 2022/09/07.
+//
+
+import Foundation
+
+func isValidSudoku(_ board: [[Character]]) -> Bool {
+  var itemCount = 0
+  var squareSet = Array(repeating: Set<Character>(), count: (board.count/3))
+  for i in 0..<board.count {
+    var rowCheckSet: Set<Character> = Set<Character>()
+    var colCheckSet: Set<Character> = Set<Character>()
+    
+    // Checking Squares, Rows, Column in sequence
+    for j in 0..<board[i].count {
+      let rowElement = board[i][j]
+      let colElement = board[j][i]
+      itemCount += 1
+      let index = j/3
+      
+      //Checking Squares
+      if checkDuplicate(val: rowElement, on: squareSet[index]) {
+        squareSet[index].insert(rowElement)
+      }else {
+        print("detected Duplicates within the square!")
+        return false
+      }
+      
+      //Checking Rows
+      if checkDuplicate(val: rowElement, on: rowCheckSet) {
+        rowCheckSet.insert(rowElement)
+      }else {
+        print("detected Duplicates within the rows!")
+        
+        return false
+      }
+      
+      //Checking Columns
+      if checkDuplicate(val: colElement, on: colCheckSet) {
+        colCheckSet.insert(colElement)
+      }else {
+        print("detected Duplicates within the columns!")
+        return false
+      }
+    }
+    
+    if itemCount % 27 == 0 {
+      for i in 0..<squareSet.count{
+        squareSet[i].removeAll()
+      }
+    }
+    
+    rowCheckSet.removeAll()
+    colCheckSet.removeAll()
+  }
+  return true
+  
+}
+
+
+func checkDuplicate(val: Character, on set: Set<Character>) -> Bool {
+  if val == "." {return true}
+  else if set.contains(val){
+    return false
+  }
+  return true
+}
